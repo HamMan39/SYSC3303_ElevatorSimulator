4bf2287d173406171f6098eff86992e466e09900
Khola Haseeb <kholahaseeb@gmail.com>
2024-02-01 20:14:20 -0500

    Added communication classes

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,2 +1,10 @@
 public class Elevator {
+    private final int nFloors;
+    private final int elevatorNumber;
+
+    public Elevator(int nFloors, int elevatorNumber){
+        this.nFloors = nFloors;
+        this.elevatorNumber = elevatorNumber;
+
+    }
 }
