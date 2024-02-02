05ee7238b891425e3eb5dd55319e9891ff77f8bd
Khola Haseeb <kholahaseeb@gmail.com>
2024-02-02 13:32:46 -0500

    added goToFloor method and updated run

--- src/ElevatorSubSystem.java
+++ src/ElevatorSubSystem.java
@@ -1,12 +1,29 @@
 public class ElevatorSubSystem implements Runnable {
     private int floor;
+    private MessageBox elevatorMessage;
+    private String message = null;
 
-    public ElevatorSubSystem(){
+    public ElevatorSubSystem(MessageBox message){
         this.floor = 0;
+        elevatorMessage = message;
+    }
+
+    public String goToFloor(String message){
+        System.out.print(message);
+        return message;
     }
 
     @Override
     public void run() {
+        while(true){
+            message = (String) elevatorMessage.get();
+            String messageRecieved = goToFloor(message);
 
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {}
+
+            elevatorMessage.put(messageRecieved);
+        }
     }
 }
