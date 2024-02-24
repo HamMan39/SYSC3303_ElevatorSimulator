a46fd0a79dc7f70302f41f85d6d76197fb07c647
mahnoor <mahnoorfatima404@gmail.com>
2024-02-23 23:30:11 -0500

    "Updated Scheduler to consider all requests at once"

--- src/ElevatorTest.java
+++ src/ElevatorTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 /**
  * Unit tests for Class Elevator
- * @author Khola Haseeb 101192363
+ * @author Khola Haseeb 101192363, Mahnoor Fatima 101192353
  */
 public class ElevatorTest {
 
@@ -10,11 +10,11 @@ public class ElevatorTest {
     public void testRun(){
 
         //create MessageBox instances for testing
-        MessageBox incommingMessages = new MessageBox();
+        MessageBox incomingMessages = new MessageBox();
         MessageBox outgoingMessages = new MessageBox();
 
         // Create Elevator instance
-        Elevator elevator = new Elevator(incommingMessages,outgoingMessages);
+        Elevator elevator = new Elevator(incomingMessages,outgoingMessages);
 
         // create thread for elevator and start it
         Thread elevatorThread = new Thread(elevator);
@@ -22,10 +22,39 @@ public class ElevatorTest {
 
         Message message = new Message("14:05:15.0",2, "Up", 4);
 
-        // Add message to incommingMessages
-        incommingMessages.put(message);
+        // Add message to incomingMessages
+        incomingMessages.put(message);
 
         // Check if outgoingMessages contains the test message
         assertEquals(message, outgoingMessages.get());
+    }
+
+    @Test
+    public void testTravelFloors(){
+        //create MessageBox instances for testing
+        MessageBox incomingMessages = new MessageBox();
+        MessageBox outgoingMessages = new MessageBox();
+
+        // Create Elevator instance
+        Elevator elevator = new Elevator(incomingMessages,outgoingMessages);
+
+        // create thread for elevator and start it
+        Thread elevatorThread = new Thread(elevator);
+        elevatorThread.start();
+
+        Message message = new Message("14:05:15.0",2, "Up", 4);
+
+        // Add message to incomingMessages
+        incomingMessages.put(message);
+
+        //Initial Floor should be 0 and not the destination floor
+        assertEquals(0, elevator.getCurrentFloor());
+        assertNotEquals(message.getDestinationFloor(), elevator.getCurrentFloor());
+
+        //Travel to the destination floor
+        elevator.travelFloors(message.getDestinationFloor());
+
+        // Check if the destination floor matches
+        assertEquals(message.getDestinationFloor(), elevator.getCurrentFloor());
     }
 }
