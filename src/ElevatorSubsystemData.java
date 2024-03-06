aeee619c2fe0a95298f428fc974de655db240d4d
Owen <opetersen1010@hotmail.com>
2024-03-06 15:44:30 -0500

    Minor changes, possible option for accessing status information

--- src/ElevatorSubsystemData.java
+++ src/ElevatorSubsystemData.java
@@ -2,22 +2,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ElevatorSubsystemData {
-    private ArrayList elevator1Details;
-    private ArrayList elevator2Details;
-    private ArrayList elevator3Details;
-    private ArrayList elevator4Details;
+    private ArrayList<ArrayList> elevatorsStatus;
+
     public enum Directions {IDLE, UP, DOWN, UPDOWN, DOWNUP}
 
 
     public ElevatorSubsystemData(){
-        elevator1Details = new ArrayList(Arrays.asList(0, Directions.IDLE, 0));
-        elevator2Details = new ArrayList(Arrays.asList(0, Directions.IDLE, 0));
-        elevator3Details = new ArrayList(Arrays.asList(0, Directions.IDLE, 0));
-        elevator4Details = new ArrayList(Arrays.asList(0, Directions.IDLE, 0));
-
-
-
+        elevatorsStatus = new ArrayList<>();
+        for (int i = 4; i > 0; i--){
+            elevatorsStatus.add(new ArrayList(Arrays.asList(0, Directions.IDLE, 0)));
+        }
     }
 
+    public synchronized void updateStatus(int elevator, ArrayList status){
+        elevatorsStatus.set(elevator, status);
+    }
+
+    //TODO either needs to ensure mutual exlcusion or alternative access to information
+    public synchronized ArrayList getStatus(){
+        return elevatorsStatus;
+    }
 
 }
