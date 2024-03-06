ef2061a1ae368847affa8816c4f623cbc2e45344
Owen <opetersen1010@hotmail.com>
2024-03-06 15:15:54 -0500

    Initialization of data storing attributes for each elevator

--- src/ElevatorSubsystemData.java
+++ src/ElevatorSubsystemData.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ElevatorSubsystemData {
+    private ArrayList elevator1Details;
+    private ArrayList elevator2Details;
+    private ArrayList elevator3Details;
+    private ArrayList elevator4Details;
+    public enum Directions {IDLE, UP, DOWN, UPDOWN, DOWNUP}
+
+
+    public ElevatorSubsystemData(){
+        elevator1Details = new ArrayList(Arrays.asList(0, Directions.IDLE, 0));
+        elevator2Details = new ArrayList(Arrays.asList(0, Directions.IDLE, 0));
+        elevator3Details = new ArrayList(Arrays.asList(0, Directions.IDLE, 0));
+        elevator4Details = new ArrayList(Arrays.asList(0, Directions.IDLE, 0));
+
+
+
+    }
+
+
 }
