01a6cc6e47f4f941d618fe175f35dff53b2bf68d
Owen <opetersen1010@hotmail.com>
2024-03-17 12:48:59 -0400

    Modified from array lists to ElevatorStatus classes

--- src/ElevatorData.java
+++ src/ElevatorData.java
@@ -2,38 +2,55 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ElevatorData {
-    private ArrayList<ArrayList> elevatorsStatus;
+    private ArrayList<ElevatorStatus> elevatorSubsystemStatus;
 
 
     public ElevatorData(){
-        elevatorsStatus = new ArrayList<>();
+        elevatorSubsystemStatus = new ArrayList<>();
         //Creates an array for all elevators to represent the current status for each
         for (int i = 4; i > 0; i--){
-            elevatorsStatus.add(new ArrayList(Arrays.asList(0, Message.Directions.IDLE, 0)));
+            elevatorSubsystemStatus.add(new ElevatorStatus());
         }
     }
 
     public boolean sameDirection(Message.Directions requestDirection, int elevator) {
-        return (requestDirection.equals(elevatorsStatus.get(elevator).get(1)));
+        return (requestDirection.equals(elevatorSubsystemStatus.get(elevator).getCurrentDirection()));
     }
     public boolean soonSameDirection(Message.Directions requestDirection, int elevator) {
         if (requestDirection.equals(Message.Directions.UP)){
-            return (elevatorsStatus.get(elevator).get(1).equals(Message.Directions.DOWNUP));
+            return (elevatorSubsystemStatus.get(elevator).getCurrentDirection().equals(Message.Directions.DOWNUP));
         } else {
-            return (elevatorsStatus.get(elevator).get(1).equals(Message.Directions.UPDOWN));
+            return (elevatorSubsystemStatus.get(elevator).getCurrentDirection().equals(Message.Directions.UPDOWN));
         }
     }
     public boolean isIdle(int elevator){
-        return (elevatorsStatus.get(elevator).get(1).equals(Message.Directions.IDLE));
+        return (elevatorSubsystemStatus.get(elevator).getCurrentDirection().equals(Message.Directions.IDLE));
     }
 
-    public synchronized void updateStatus(int elevator, ArrayList status){
-        elevatorsStatus.set(elevator, status);
+    /**
+     * updates each elevatorStatus with the information in the byte array
+     * @param statusBytes A byte array version of another elevatorData class
+     */
+    public synchronized void updateStatus(byte[] statusBytes){
+        return;
     }
 
-    //TODO either needs to ensure mutual exlcusion or alternative access to information
-    public synchronized ArrayList getStatus(){
-        return elevatorsStatus;
-    }
 
+    class ElevatorStatus {
+        private int currentFloor;
+        private Message.Directions currentDirection;
+        private int currentLoad; //Number of passengers being carried (cannot exceed capacity)
+
+        public ElevatorStatus(){
+            currentFloor = 0;
+            currentDirection = Message.Directions.IDLE;
+            currentLoad = 0;
+        }
+        public int getCurrentFloor(){return currentFloor;}
+        public Message.Directions getCurrentDirection(){return currentDirection;}
+        public int getCurrentLoad(){return currentLoad;}
+        public void setCurrentFloor(int newFloor){currentLoad = newFloor;}
+        public void setCurrentDirection(Message.Directions newDirection){currentDirection = newDirection;}
+        public void setCurrentLoad(int newLoad){currentLoad = newLoad;}
+    }
 }
