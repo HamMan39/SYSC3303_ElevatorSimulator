93104bb1d2920dd0d253d876f564ddbe10007e31
Owen <opetersen1010@hotmail.com>
2024-03-17 13:05:28 -0400

    Added converting instance to byte[] and updating instance from a byte[]

--- src/ElevatorData.java
+++ src/ElevatorData.java
@@ -1,3 +1,6 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -32,25 +35,55 @@ public class ElevatorData {
      * @param statusBytes A byte array version of another elevatorData class
      */
     public synchronized void updateStatus(byte[] statusBytes){
-        return;
+        //Create an input stream to parse the byte array
+        ByteArrayInputStream inStream = new ByteArrayInputStream(statusBytes);
+
+        for (ElevatorStatus elevator: elevatorSubsystemStatus) {
+            elevator.setCurrentFloor(inStream.read());
+            elevator.setCurrentDirection(Message.Directions.values()[inStream.read()]);
+            elevator.setCurrentFloor(inStream.read());
+        }
     }
 
+    public byte[] toByteArray(){
 
-    class ElevatorStatus {
-        private int currentFloor;
-        private Message.Directions currentDirection;
-        private int currentLoad; //Number of passengers being carried (cannot exceed capacity)
-
-        public ElevatorStatus(){
-            currentFloor = 0;
-            currentDirection = Message.Directions.IDLE;
-            currentLoad = 0;
+        ByteArrayOutputStream byteArrayBuilder = new ByteArrayOutputStream();
+        try {
+            for (ElevatorStatus elevator: elevatorSubsystemStatus) {
+                byteArrayBuilder.write(elevator.toByteArray());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
         }
-        public int getCurrentFloor(){return currentFloor;}
-        public Message.Directions getCurrentDirection(){return currentDirection;}
-        public int getCurrentLoad(){return currentLoad;}
-        public void setCurrentFloor(int newFloor){currentLoad = newFloor;}
-        public void setCurrentDirection(Message.Directions newDirection){currentDirection = newDirection;}
-        public void setCurrentLoad(int newLoad){currentLoad = newLoad;}
+
+        return byteArrayBuilder.toByteArray();
+
+    }
+}
+class ElevatorStatus {
+    private int currentFloor;
+    private Message.Directions currentDirection;
+    private int currentLoad; //Number of passengers being carried (cannot exceed capacity)
+
+    public ElevatorStatus(){
+        currentFloor = 0;
+        currentDirection = Message.Directions.IDLE;
+        currentLoad = 0;
+    }
+    public int getCurrentFloor(){return currentFloor;}
+    public Message.Directions getCurrentDirection(){return currentDirection;}
+    public int getCurrentLoad(){return currentLoad;}
+    public void setCurrentFloor(int newFloor){currentLoad = newFloor;}
+    public void setCurrentDirection(Message.Directions newDirection){currentDirection = newDirection;}
+    public void setCurrentLoad(int newLoad){currentLoad = newLoad;}
+    public byte[] toByteArray(){
+        ByteArrayOutputStream byteArrayBuilder = new ByteArrayOutputStream();
+
+        byteArrayBuilder.write(currentFloor);
+        byteArrayBuilder.write(currentDirection.ordinal());
+        byteArrayBuilder.write(currentLoad);
+
+        return byteArrayBuilder.toByteArray();
     }
 }
