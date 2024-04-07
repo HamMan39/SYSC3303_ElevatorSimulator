c508371e3e82852d5f4bf433ea48bdaf13a421d3
Owen <opetersen1010@hotmail.com>
2024-04-07 14:08:36 -0400

    added constant for the maximum capacity of the elevator

--- src/Elevator.java
+++ src/Elevator.java
@@ -13,6 +13,7 @@ import java.util.concurrent.TimeoutException;
  */
 public class Elevator extends CommunicationRPC implements Runnable {
 
+    public static final int MAX_CAPACITY = 5;
     public enum state{IDLE, MOVING, DOOR_OPEN, DOOR_CLOSED, DOOR_STUCK, DISABLED}
 
     private state currentState;
